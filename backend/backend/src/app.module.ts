5d7d5f7e72a9fe85ed805fb28b5b484405a1f18f
Yasarahasini <yasarahasini61@gmail.com>
2026-02-01 12:01:28 +0530

    setup auth backend

--- backend/backend/src/app.module.ts
+++ backend/backend/src/app.module.ts
@@ -1,4 +1,3 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
--- backend/backend/src/users/user.entity.ts
+++ backend/backend/src/users/user.entity.ts
@@ -1,4 +1,3 @@
-// src/users/user.entity.ts
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
