e1fbfca542ad6e63a05da40af7ba7fcea8705993
Yasarahasini <yasarahasini61@gmail.com>
2026-02-01 10:07:10 +0530

    update help pagw

--- frontend/app/(user)/help/page.tsx
+++ frontend/app/(user)/help/page.tsx
@@ -55,37 +55,37 @@ const HelpContactPage: React.FC = () => {
         </div>
 
         
-        <div className="bg-white text-blue-500 rounded-xl shadow p-6">
+        <div className="bg-green-900 text-white rounded-xl shadow p-6">
           <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
 
-          <form className="space-y-4">
+          <form className="space-y-4 text-white">
             <input
               type="text"
               placeholder="Your Name"
-              className="w-full border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
+              className="w-full border rounded-md px-4 py-2 focus:outline-none focus:ring-2 text-white focus:ring-black"
             />
 
             <input
               type="email"
               placeholder="Your Email"
-              className="w-full border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
+              className="w-full border rounded-md px-4 py-2 focus:outline-none focus:ring-2 text-white focus:ring-black"
             />
 
             <textarea
               placeholder="Your Message"
               rows={5}
-              className="w-full border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-black"
+              className="w-full border rounded-md px-4 py-2 focus:outline-none focus:ring-2 text-white focus:ring-black"
             ></textarea>
 
             <button
               type="submit"
-              className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition"
+              className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 text-white transition"
             >
               Send Message
             </button>
           </form>
 
-          <div className="mt-6 text-sm text-gray-600 space-y-1">
+          <div className="mt-6 text-sm text-white space-y-1">
             <p>📧 Email: support@yashstore.com</p>
             <p>📞 Phone: +94 71 054 9560</p>
             <p>📍 Address: Colombo, Sri Lanka</p>
