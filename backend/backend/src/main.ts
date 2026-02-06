cac53437905081d89f514646124d592418e957d3
Yasarahasini <yasarahasini61@gmail.com>
2026-02-06 21:21:43 +0530

    change  backend

--- backend/backend/src/main.ts
+++ backend/backend/src/main.ts
@@ -4,14 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
- 
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
 
- 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(3001);
