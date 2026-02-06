07b0014a54ee3b5067063bd443dceadc1f6e79b9
Yasarahasini <yasarahasini61@gmail.com>
2026-02-06 21:20:52 +0530

    change contact page

--- backend/backend/src/main.ts
+++ backend/backend/src/main.ts
@@ -5,13 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS allow frontend
+ 
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
 
-  // Enable DTO validation
+ 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(3001);
