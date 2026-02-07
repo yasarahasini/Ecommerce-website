d07fe27c5d83828155e327d744a413e2077f3230
Yasarahasini <yasarahasini61@gmail.com>
2026-02-07 21:54:53 +0530

    update electronic backend

--- backend/backend/src/electronic/electronic.service.ts
+++ backend/backend/src/electronic/electronic.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Electronic } from './entities/electronic.entity';
+import { Electronic } from './entity/electronic.entity';
 import { CreateElectronicDto } from './dto/create-electronic.dto';
 
 @Injectable()
