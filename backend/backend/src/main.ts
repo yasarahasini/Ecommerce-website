674d307247d1badd81a380d7dd40c125cb46ddef
Yasarahasini <yasarahasini61@gmail.com>
2026-02-09 19:19:25 +0530

    update pages

--- backend/backend/src/main.ts
+++ backend/backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3002',
     credentials: true,
   });
 
