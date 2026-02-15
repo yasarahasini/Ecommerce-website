382bcd223ee624d1daf1d57491fc0d35cac6f0ef
Yasarahasini <yasarahasini61@gmail.com>
2026-02-15 19:52:07 +0530

    create backend of book

--- backend/backend/src/app.module.ts
+++ backend/backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { AboutModule } from './about/about.module';
 import { AntiquesModule } from './antiques/antiques.module';
 import { ArtModule } from './art/art.module';
 import { BabyModule } from './baby/baby.module';
+import { BookModule } from './book/book.module';
 
 @Module({
   imports: [
@@ -39,6 +40,7 @@ import { BabyModule } from './baby/baby.module';
     AntiquesModule,
     ArtModule,
     BabyModule,
+    BookModule,
   ],
 })
 export class AppModule {}
