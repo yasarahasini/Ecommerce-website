ad366c4b72d900dc6d594814c2cdb3a6bcd10bab
Yasarahasini <yasarahasini61@gmail.com>
2026-02-16 09:45:33 +0530

    create utility

--- frontend/app/utility/auth.ts
+++ frontend/app/utility/auth.ts
@@ -1,26 +1,26 @@
-// utils/auth.ts
+
 
 export const TOKEN_KEY = "token";
 
-// ✅ Check user logged in or not
+
 export const isAuthenticated = (): boolean => {
   if (typeof window === "undefined") return false;
   return !!localStorage.getItem(TOKEN_KEY);
 };
 
-// ✅ Get token
+
 export const getToken = (): string | null => {
   if (typeof window === "undefined") return null;
   return localStorage.getItem(TOKEN_KEY);
 };
 
-// ✅ Save token after login
+
 export const loginUser = (token: string) => {
   if (typeof window === "undefined") return;
   localStorage.setItem(TOKEN_KEY, token);
 };
 
-// ✅ Logout user
+
 export const logoutUser = () => {
   if (typeof window === "undefined") return;
   localStorage.removeItem(TOKEN_KEY);
