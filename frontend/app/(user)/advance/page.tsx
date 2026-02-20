e0772ea6a8cf07ed016c065d19c6ff7de1d69856
Yasarahasini <yasarahasini61@gmail.com>
2026-02-20 10:58:34 +0530

    update pages

--- frontend/app/(user)/advance/page.tsx
+++ frontend/app/(user)/advance/page.tsx
@@ -9,6 +9,33 @@ const page = () => {
           Explore our advanced features designed to enhance your shopping experience.
         </p>
       </div>
+<div className='flex float-left flex-wrap gap-4 justify-center mt-10 items-center px-100'>
+      <div className='bg-red-500 w-50 h-30 py-12 px-4 mt-9 rounded-b-4xl items-center justify-center text-center'>
+<div>
+    New Arrivals
+</div>
+      </div>
+         <div className='bg-red-500 w-50 h-30 py-12 px-4 mt-9 rounded-b-4xl items-center justify-center text-center'>
+<div>
+    Deals
+</div>
+      </div>
+         <div className='bg-red-500 w-50 h-30 py-12 px-4 mt-9 rounded-b-4xl items-center justify-center text-center'>
+<div>
+    Brand Outlet
+</div>
+      </div>
+         <div className='bg-red-500 w-50 h-30 py-12 px-4 mt-9 rounded-b-4xl items-center justify-center text-center'>
+<div>
+    Gift Cards
+</div>
+      </div>
+         <div className='bg-red-500 w-50 h-30 py-12 px-4 mt-9 rounded-b-4xl items-center justify-center text-center'>
+<div>
+   Feactures
+</div>
+      </div>
+      </div>
     </div>
   )
 }
