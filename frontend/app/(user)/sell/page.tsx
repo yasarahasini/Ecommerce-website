dda46c65b88cd77766f95b97ca5850f70f43e580
Yasarahasini <yasarahasini61@gmail.com>
2026-02-20 10:41:42 +0530

    update pages

--- frontend/app/(user)/sell/page.tsx
+++ frontend/app/(user)/sell/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function ProductPage() {
   return (
 
-    <div>
+    <div className="bg-gradient-to-tr from-gray-300 via-white to-gray-400">
     <div className="max-w-6xl mx-auto p-4">
     
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
